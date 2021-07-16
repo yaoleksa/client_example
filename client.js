68cd293acd2994ef1acdad5860920650a08e3402
PavloMartovichPowerSnow <mryaremchyk@gmail.com>
2021-07-16 11:28:03 +0300

    Comment

---
+++ client.js
@@ -0,0 +1,32 @@
+const req = new XMLHttpRequest();
+const p = document.createElement('p');
+
+document.body.appendChild(p);
+
+const state = {
+    0: 'UNSENT',
+    1: 'OPENED',
+    2: 'HEADERS_RECEIVED',
+    3: 'LOADING',
+    4: 'DONE'
+}
+
+req.onreadystatechange = (event) => {
+    p.innerHTML = state[req.readyState];
+}
+
+req.onerror = (event) => {
+    console.log(event);
+}
+
+req.onload = (event) => {
+    p.innerHTML = req.response;
+}
+
+req.open('POST', 'http://localhost:5000');
+req.setRequestHeader('Content-Type', 'text/plain');
+req.send('msg');
+
+req.open('GET', 'http://localhost:5000');
+req.setRequestHeader('Content-Type', 'text/plain');
+req.send();
